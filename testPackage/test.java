39073bab6c0b4e87f99406a67ebae89dd334a070
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2015-12-08 13:00:10 +0800

    not using import

--- testPackage/test.java
+++ testPackage/test.java
@@ -1,11 +1,11 @@
- import com.company.src;
+ //import com.company.src;
  class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         aTest a = new aTest();
         a.f(0);
-        src b = new src();
+        com.company.src b = new com.company.src();
         b.g(1);
         System.out.println("Hello, test no public");
     }
