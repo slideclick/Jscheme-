c419150093e5b3837886773c43dd4cc24b4888db
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2015-12-08 13:05:28 +0800

    一个class不必和文件同名，如果你不public它 一个method不必public如果你仅仅是本package访问它，比如你的小项目没有package

--- testPackage/aTest.java
+++ testPackage/aTest.java
@@ -1,10 +1,18 @@
- class aTest {
+ class atest {
+
+
+     void f(int n)//private
+    {
+        System.out.println("Hello, f");
+    }
+}
+ class ddd {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, aTest no public");
     }
-    void f(int n)
+     void f(int n)//private
     {
         System.out.println("Hello, f");
     }
--- testPackage/test.java
+++ testPackage/test.java
@@ -3,7 +3,7 @@
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        aTest a = new aTest();
+        atest a = new atest();
         a.f(0);
         com.company.src b = new com.company.src();
         b.g(1);
