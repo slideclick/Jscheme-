f733885e0ff69e9d944f16cab769ec64bb599a5e
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2015-12-08 12:59:03 +0800

    success access package

--- testPackage/com/company/src.java
+++ testPackage/com/company/src.java
@@ -1,8 +1,12 @@
- class src {
+ package com.company;
+  public class src {
 
     public static void main(String[] args) {
 
         System.out.println("Hello, src no public");
     }
-
+    public void g(int n)
+    {
+        System.out.println("Hello, gg");
+    }
 }
--- testPackage/test.java
+++ testPackage/test.java
@@ -1,9 +1,12 @@
+ import com.company.src;
  class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         aTest a = new aTest();
         a.f(0);
+        src b = new src();
+        b.g(1);
         System.out.println("Hello, test no public");
     }
 
